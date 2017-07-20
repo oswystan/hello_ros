5b62f39324b0667d5493b7d22fa36cc00f42632f
oswystan <oswystan@126.com>
2017-07-20 16:01:44 +0800

    refactor: rename node name to node_hello

--- hello/src/hello.cpp
+++ hello/src/hello.cpp
@@ -13,7 +13,7 @@
 #include <ros/ros.h>
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "hello");
+    ros::init(argc, argv, "node_hello");
     ros::NodeHandle nh;
     ROS_INFO("hello, ros\n");
     return 0;
